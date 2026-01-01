22f43c70e17391ca4a9428243c440f1884e591bc
UDAY KIRAN TIPPANI <152371472+Uday-kiran-Tippani@users.noreply.github.com>
2026-01-01 21:50:00 +0530

    Fix indentation for app.run in app.py

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ if __name__ == "__main__":
     # debug=True automatically reloads app when code changes
     # app.run(debug=True)
      port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+     app.run(host="0.0.0.0", port=port)
